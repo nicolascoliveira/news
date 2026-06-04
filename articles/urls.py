911b54770563699e5c8ad4d23fa087185065999f
Nicolas Oliveira <nicolasoliveira1902@gmail.com>
2026-06-04 19:38:16 -0300

    Autorizacao e permissoes

--- articles/urls.py
+++ articles/urls.py
@@ -3,7 +3,7 @@ from .views import ArticleListView, ArticleDetailView, ArticleUpdateView, Articl
 
 urlpatterns = [
     path('', ArticleListView.as_view(), name='article_list'),
-    path('create/', ArticleCreateView.as_view(), name='article_new'),
+    path('new/', ArticleCreateView.as_view(), name='article_new'),
     path('<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
     path('<int:pk>/update/', ArticleUpdateView.as_view(), name='article_edit'),
     path('<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'),
